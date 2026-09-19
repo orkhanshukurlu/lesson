e3c4dac0720b0145b91146ce93c17ac3a0781e1c
Orkhan Shukurlu <orkhandev@gmail.com>
2026-09-20 02:38:27 +0400

    Add 8 more lessons

--- 72-json-tags/main.go
+++ 72-json-tags/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age,omitempty"`
+}
+
+func main() {
+	p := Person{Name: "John", Age: 30}
+	data, _ := json.Marshal(p)
+	fmt.Println(string(data))
+
+	p2 := Person{Name: "Jane"}
+	j, _ := json.Marshal(p2)
+	fmt.Println(string(j))
 }
