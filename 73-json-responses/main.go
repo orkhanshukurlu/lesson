de4a1e2c55b1334a53973c1f69e14a54b37dd144
Orkhan Shukurlu <orkhandev@gmail.com>
2026-09-20 23:47:52 +0400

    Add lesson about json responses

--- 73-json-responses/main.go
+++ 73-json-responses/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+func main() {
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		user := User{
+			Name:  "John Doe",
+			Email: "john.doe@example.com",
+			Age:   30,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		err := json.NewEncoder(w).Encode(user)
+		if err != nil {
+			http.Error(w, "Error has occurred", http.StatusInternalServerError)
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
---
+++ 83-select-statement/go.mod
@@ -0,0 +1,3 @@
+module select-statement
+
+go 1.27.1
